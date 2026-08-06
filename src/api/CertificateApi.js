346759ce2b9f06fc1635c480fc298c9b17a68166
Tenzin Tamang <tenzintamang@Tenzins-MacBook-Air-606.local>
2026-08-07 01:04:28 +0530

    Update production API base URL

--- src/api/CertificateApi.js
+++ src/api/CertificateApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://certifypro-1-ptki.onrender.com",
 });
 
 // Automatically attach JWT token
